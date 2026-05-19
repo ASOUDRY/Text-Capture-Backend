a2cfcfa15a264149d82ea01cbecf29ff38367916
Alexander Soudry <soudryalex@gmail.com>
2026-05-18 21:43:03 -0400

    initial commit

---
+++ src/main/java/com/soudry/text_extension/TextExtensionApplication.java
@@ -0,0 +1,13 @@
+package com.soudry.text_extension;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextExtensionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextExtensionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soudry/text_extension/TextExtensionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soudry.text_extension;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextExtensionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
